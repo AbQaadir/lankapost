671f3f1c47a1562ef0ccf63f9902b40d75c2c277
AbQaadir <qaadireng@gmail.com>
2026-09-01 08:48:54 +0530

    chore: bump version to 1.0.1 across Python package

--- lankapost/__init__.py
+++ lankapost/__init__.py
@@ -9,7 +9,7 @@ import os
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Union
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = [
     "getAll",
     "getByPostalCode",
